22cace872caf4b2c9968ce608d78d2b7affe72b6
KRISHNA KUMAR VERMA <155811536+wd369018@users.noreply.github.com>
2026-07-26 16:37:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+ var typed = new Typed(".text", {
+    strings: ["Data Analyst", "MySQL Developer", "Power BI Expert", "Tableau Specialist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop: true
+});
